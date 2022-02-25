c6ca63221ac60abfecdba51ea06cf1c29ffbcfee
nitiniosdev <88797873+nitiniosdev@users.noreply.github.com>
2022-02-25 21:12:58 +0530

    added home page navigation title

--- DemoProject2/DemoProject2/AppDelegate.swift
+++ DemoProject2/DemoProject2/AppDelegate.swift
@@ -13,7 +13,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    
+        print("Hello world")
+        
         return true
     }
 
