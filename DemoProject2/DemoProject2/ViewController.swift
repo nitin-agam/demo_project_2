a5676a16d7b25767558438cb6c1a1753930f1d4d
nitiniosdev <88797873+nitiniosdev@users.noreply.github.com>
2022-02-25 20:56:02 +0530

    project created

---
+++ DemoProject2/DemoProject2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoProject2
+//
+//  Created by Nitin Aggarwal on 25/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
